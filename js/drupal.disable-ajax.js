5d3290232e50d6bcb83606dfbd06a6c24c110d01
Jon Duell <jon@jonduell.com>
2013-09-18 15:45:42 -0700

    Adds js to disable submit buttons during ajax requests

---
+++ js/drupal.disable-ajax.js
@@ -0,0 +1,25 @@
+(function ($) {
+
+  Drupal.behaviors.OmbuCleanupDisableAjax = {
+    attach: function(context, settings) {
+      for (ajax_el in settings.ajax) {
+        if (Drupal.ajax[ajax_el] && Drupal.ajax[ajax_el].element.form) {
+          Drupal.ajax[ajax_el].beforeSubmit = Drupal.OmbuCleanupAjax.beforeSubmit;
+        }
+      }
+    }
+  };
+
+  Drupal.behaviors.OmbuCleanupEnableSubmit = {
+    attach: function(context, settings) {
+      $('input[type=submit]').removeAttr('disabled').fadeTo(200, 1);
+    }
+  };
+
+  Drupal.OmbuCleanupAjax = {};
+
+  Drupal.OmbuCleanupAjax.beforeSubmit = function (form_values, form, options) {
+    $('input[type=submit]', form).attr('disabled', 'disabled').fadeTo(200, .20);
+  }
+
+})(jQuery);
